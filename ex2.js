98a45857e50069884a9e6d4a6e2e5b925932d481
Augustin Le Fèvre <aulefevre@kilix.fr>
2017-12-30 16:09:22 +0100

    ex2.2

--- ex2.js
+++ ex2.js
@@ -1,11 +1,31 @@
 const {parseFile} = require("./parseFile");
 const getLineMinMax = line => [Math.min(...line), Math.max(...line)];
-const calculateRowCheckSum = line => {
+const calculateRowMinMaxCheckSum = line => {
     const [min, max] = getLineMinMax(line);
     return max - min;
 };
-const calculateSpreadsheetCheckSum = spreadsheet =>
+const calculateRowDividedCheckSum = line => {
+    const sortedLine = [...line].sort((a, b) => a - b);
+    for (let i = sortedLine.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            const first = sortedLine[i];
+            const second = sortedLine[j];
+            const divisionResult = first / second;
+            if (Number.isInteger(divisionResult)) return divisionResult;
+        }
+    }
+    throw new Error("No number found");
+};
+
+const getSpreadsheetCheckSumCalculator = calculateRowCheckSum => spreadsheet =>
     spreadsheet.reduce((sum, line) => sum + calculateRowCheckSum(line), 0);
+
+const calculateSpreadsheetMinMaxCheckSum = getSpreadsheetCheckSumCalculator(
+    calculateRowMinMaxCheckSum
+);
+const calculateSpreadsheetDivisionCheckSum = getSpreadsheetCheckSumCalculator(
+    calculateRowDividedCheckSum
+);
 
 const parsePuzzleInput = input =>
     parseFile(input)
@@ -14,7 +34,9 @@ const parsePuzzleInput = input =>
 
 module.exports = {
     getLineMinMax,
-    calculateRowCheckSum,
-    calculateSpreadsheetCheckSum,
+    calculateRowDividedCheckSum,
+    calculateRowMinMaxCheckSum,
+    calculateSpreadsheetMinMaxCheckSum,
+    calculateSpreadsheetDivisionCheckSum,
     parsePuzzleInput,
 };
--- ex2.spec.js
+++ ex2.spec.js
@@ -1,23 +1,47 @@
-const {calculateRowCheckSum, calculateSpreadsheetCheckSum, parsePuzzleInput} = require("./ex2");
+const {
+    calculateRowMinMaxCheckSum,
+    calculateRowDividedCheckSum,
+    calculateSpreadsheetMinMaxCheckSum,
+    calculateSpreadsheetDivisionCheckSum,
+    parsePuzzleInput,
+} = require("./ex2");
 
 describe("ex2.1", () => {
-    describe("calculateRowCheckSum", () => {
+    describe("calculateRowMinMaxCheckSum", () => {
         it("5 1 9 5", () => {
-            expect(calculateRowCheckSum([5, 1, 9, 5])).toBe(8);
+            expect(calculateRowMinMaxCheckSum([5, 1, 9, 5])).toBe(8);
         });
         it("7 5 3", () => {
-            expect(calculateRowCheckSum([7, 5, 3])).toBe(4);
+            expect(calculateRowMinMaxCheckSum([7, 5, 3])).toBe(4);
         });
         it("2 4 6 8", () => {
-            expect(calculateRowCheckSum([2, 4, 6, 8])).toBe(6);
+            expect(calculateRowMinMaxCheckSum([2, 4, 6, 8])).toBe(6);
         });
     });
-    it("calculateSpreadsheetCheckSum", () => {
+    it("calculateSpreadsheetMinMaxCheckSum", () => {
         const spreadsheet = [[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]];
-        expect(calculateSpreadsheetCheckSum(spreadsheet)).toBe(18);
+        expect(calculateSpreadsheetMinMaxCheckSum(spreadsheet)).toBe(18);
     });
     it("should compute the puzzle input checksum", () => {
         const spreadsheet = parsePuzzleInput("./ex2.txt");
-        expect(calculateSpreadsheetCheckSum(spreadsheet)).toBe(45158);
+        expect(calculateSpreadsheetMinMaxCheckSum(spreadsheet)).toBe(45158);
+    });
+});
+
+describe("ex2.2", () => {
+    describe("calculateRowDividedCheckSum", () => {
+        it("5 9 2 8", () => {
+            expect(calculateRowDividedCheckSum([5, 9, 2, 8])).toBe(4);
+        });
+        it("9 4 7 3", () => {
+            expect(calculateRowDividedCheckSum([9, 4, 7, 3])).toBe(3);
+        });
+        it("3 8 6 5", () => {
+            expect(calculateRowDividedCheckSum([3, 8, 6, 5])).toBe(2);
+        });
+    });
+    it("should compute the puzzle input checksum", () => {
+        const spreadsheet = parsePuzzleInput("./ex2.txt");
+        expect(calculateSpreadsheetDivisionCheckSum(spreadsheet)).toBe(294);
     });
 });
